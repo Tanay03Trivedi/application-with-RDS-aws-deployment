3576014475caa39f0c5924e8eb46408fe514b4ef
Tanay Trivedi <160705084+Tanay03Trivedi@users.noreply.github.com>
2024-05-15 23:34:16 +0530

    Add files via upload

---
+++ python-data-application/config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cn0yq6a6wqlo.ap-south-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "admin123"
+customdb = "employee"
+custombucket = "mytestdatastore"
+customregion = "ap-south-1"
